e470665eed04889472595e787f66991ef87677b4
Your GitHub Username <your-email@example.com>
2026-08-29 02:15:15 +0530

    Update code

--- back/server.js
+++ back/server.js
@@ -40,8 +40,12 @@ app.get('/enquiry', (req, res) => {
   res.render('enquiry');
 });
 
+app.get('/resume', (req, res) => {
+  res.render('resume');
+});
+
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
